2aa48263e4e01b980f08af08f6f770d4b09076be
zaineb <elyasmizaineb26@gmail.com>
2021-11-11 13:07:23 +0100

    new

---
+++ conn.php
@@ -0,0 +1,36 @@
+<?php
+
+
+
+$conn = mysqli_connect("localhost","root","","dentaire");
+
+mysqli_query($conn,"SET NAMES 'utf8'");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+
+  
+?>
+<!-- 
+<div class="col-md-6 col-sm-8 clearfix">
+                        <div class="nav-btn pull-left">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                       
+                    </div>
+
+                    <div class="user-profile pull-right">
+                            <img class="avatar user-thumb" src="assets/images/author/avatar.png" alt="avatar">
+                            <h4 class="user-name dropdown-toggle" data-toggle="dropdown">Kumkum Rai <i class="fa fa-angle-down"></i></h4>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="#">Message</a>
+                                <a class="dropdown-item" href="#">Settings</a>
+                                <a class="dropdown-item" href="#">Log Out</a>
+                       
+                </div> -->
---
+++ header.php
@@ -0,0 +1,28 @@
+
+	<div class="header">
+		<div class="header-left">
+			<div class="menu-icon dw dw-menu"></div>
+		
+		</div>
+		<div class="header-right">
+			<div class="dashboard-setting user-notification">
+				
+			</div>
+		
+			<div class="user-info-dropdown">
+				<div class="dropdown">
+					<a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+						<span class="user-icon">
+							<img src="vendors/images/avatar3.png" alt="">
+						</span>
+						<span class="user-name">Bienvenue</span>
+					</a>
+					<div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+				
+						<a class="dropdown-item" href="logout.php"><i class="dw dw-logout"></i> Déconnexion</a>
+					</div>
+				</div>
+			</div>
+	
+		</div>
+	</div>
